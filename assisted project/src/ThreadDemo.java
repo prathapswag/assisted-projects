26cc77a12da6bde0f4aac7ac3293c109e2198a91
prathapswag <PRATHAP@LAPTOP-1NE3DOMM>
2022-02-24 10:39:21 +0530

    phase1 assisted projects

---
+++ assisted project/src/ThreadDemo.java
@@ -0,0 +1,14 @@
+
+
+public class ThreadDemo extends Thread
+{
+	public void run()
+	{
+	 System.out.println("concurrent thread started running..");
+	}
+	public static void main( String args[] )
+	{
+	 ThreadDemo thread = new ThreadDemo();
+	 thread.start();
+    }
+}
